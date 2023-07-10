53670e2f9b0ea0498e72d5a84f8ed519a5bce0e1
Sergio <119548442+sergiorodriguezdev@users.noreply.github.com>
2023-07-09 23:27:48 -0600

    Add app install code

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,22 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
+let installEvent;
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  butInstall.style.visibility = "visible";
+  installEvent = event;
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener("click", async () => {
+    installEvent.prompt();
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener("appinstalled", (event) => {
+  console.log("App has been installed", event);
+  butInstall.setAttribute("disabled", true);
+  butInstall.textContent = "Installed!";
+});
