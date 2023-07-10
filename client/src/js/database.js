615bfb4c849d6ba656166dd6e9f833a9b27155a1
Sergio <119548442+sergiorodriguezdev@users.noreply.github.com>
2023-07-09 22:36:16 -0600

    Update CRUD code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,15 +14,19 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
+  console.log('Saving to jate database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
+  const clearRequest = store.clear();
+  const clearResult = await clearRequest;
   const request = store.add({content});
   const result = await request;
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
+  console.log('Retrieving data from jate database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
